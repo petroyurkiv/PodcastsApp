83f596abaabe00380a69cd687994826d997907b2
petroyurkiv <igorlamativ@gmail.com>
2023-05-18 14:56:08 +0300

    code after video

--- PodcastsApp/ViewController.swift
+++ PodcastsApp/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Genres: Decodable {
+    let genres: [Genre]
+}
+
+struct Genre: Decodable {
+    let id: Int
+    let name: String
+    let parentID: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case parentID = "parent_id"
+    }
+}
+
+class ViewController: UITableViewController {
+    
+    var models: [Genre] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        getGenres()
     }
 
+    private func getGenres() {
+        var request = URLRequest(url: URL(string: "https://listen-api-test.listennotes.com/api/v2/genres")!)
+        request.httpMethod = "GET"
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            
+            do {
+                let result = try JSONDecoder().decode(Genres.self, from: data)
+                print("\(result)")
+                self.models = result.genres
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")!
+        let models = models[indexPath.row]
+        cell.textLabel?.text = models.name
+        return cell
+    }
 
 }
 
